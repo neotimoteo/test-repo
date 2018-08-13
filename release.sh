68951479031273bfc37787629a79c30f02eae511
Timoteo Latisnere <tlatisnere@neology.mx>
2018-08-13 18:30:06 -0500

    [BN-999] improvement Added release script

---
+++ release.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+
+# release.sh
+
+# Set default branch names and suffixes
+DEVELOP_BRANCH="develop"
+MASTER_BRANCH="master"
+RELEASE_SUFFIX="rc"
+FIX_TYPE="fix"
+HOTFIX_TYPE="hotfix"
+IMPROVEMENT_TYPE="improvement"
+FEATURE_TYPE="feature"
+GENERIC_TYPE="n/a"
+VERSION_FILE="version.txt"
+
+function print_usage() {
+    echo "Usage: release.sh OPTION [ARGUMENTS...]" >&2
+    echo "Description: Creates a new release candidate branch or upgrades current release version. " >&2
+    echo "OPTIONS: " >&2
+    echo "-c Create release. " >&2
+    echo "   Arguments: " >&2
+    echo "     message: Will be used as git tag message." >&2
+    echo "-u Upgrade current release. To use this option, current branch must be a release candidate. " >&2
+    exit 1
+}
+
+function validate_arguments() {
+    if [ "$#" -eq 0 ]; then
+        print_usage
+    elif [ "${1}" == "-c" ] && [ "$#" -lt 2 ]; then
+        echo "A message describing the new release must be supplied when using option -c" >&2
+        exit 1
+    fi
+}
+
+function get_last_version() {
+    # Recover version data from Git
+    last_tag=$(git describe --abbrev=0)
+    last_version=$(echo ${last_tag} | grep -oP "v\K\d+\.\d\.\d\.\d(?=[\-\w]*)")
+    last_label=$(echo ${last_tag} | grep -oP "v\d+\.\d\.\d\.\d\-\K\w*")
+    current_branch=$(git branch | grep -oP "\*\s\K.+")
+    last_commit_message=$(git log --format=%B -n 1)
+    last_commit_info=($(echo "${last_commit_message}" | tr ' ' '\n'))
+    # Possible commit types: fix, hotfix, improvement, feature, n/a
+    last_commit_type=${last_commit_info[1]}
+
+    # Extract version components in the format W.X.Y.Z
+    version_components=($(echo "${last_version}" | tr '.' '\n'))
+}
+
+function update_version_file() {
+    local new_version="${1}"
+    sed "s/${last_tag}/v${new_version}/" ${VERSION_FILE} > tmp.txt && mv tmp.txt ${VERSION_FILE}
+}
+
+function create_release_candidate() {
+    local tag_message="${1}"
+    # Create a release candidate only when develop is the origin
+    if [ ${current_branch} == ${DEVELOP_BRANCH} ]; then
+        # Increase version 'X' component with each new release. Reset to zero 'Y' and 'Z' components.
+        release_version=$(echo "${version_components[0]}."$((${version_components[1]} + 1))".0.0")
+        release_version=$(echo "${release_version}-${RELEASE_SUFFIX}")
+        # Tag develop commit from which release is created
+        git tag -a "v${release_version}" -m "${tag_message}"
+        git push origin --tags
+        # Create release branch
+        git checkout -b "${release_version}"
+        # Update version file
+        update_version_file "${release_version}"
+        git add ${VERSION_FILE}
+        git commit -m "[n/a] n/a Release ${release_version} branch created."
+        git push origin "${release_version}"
+    else
+        echo "Release candidate branches should only be created from develop branch!" >&2
+        echo "Aborting release creation." >&2
+        exit 1
+    fi
+}
+
+function upgrade_release_candidate() {
+    if [ ${last_commit_type} == ${FIX_TYPE} ] || [ ${last_commit_type} == ${HOTFIX_TYPE} ]; then
+        # Increase version 'Y' component with each bug fix. Reset 'Z' to zero
+        release_version=$(echo "${version_components[0]}.${version_components[1]}."$((${version_components[2]} + 1))".0")
+    elif [ ${last_commit_type} == ${IMPROVEMENT_TYPE} ] || [ ${last_commit_type} == ${GENERIC_TYPE} ]; then
+        # Increase version 'Z' component with each improvement.
+        release_version=$(echo "${version_components[0]}.${version_components[1]}.${version_components[2]}."$((${version_components[3]} + 1)))
+    elif [ ${last_commit_type} == ${FEATURE_TYPE} ]; then
+        # New features should not be allowed in release branches.
+        echo "Commits associated to new functionality should not be applied to release branches." >&2
+        echo "Aborting release upgrade." >&2
+        exit 1
+    fi
+    release_version=$(echo "${release_version}-${RELEASE_SUFFIX}")
+    update_version_file "${release_version}"
+    git add "${VERSION_FILE}"
+    git commit -m "[n/a] n/a Release ${release_version} upgrade applied."
+    last_commit_message=$(git log --format=%B -n 1)
+    git tag -a "v${release_version}" -m "${last_commit_message}"
+    git push origin "${current_branch}"
+    git push origin --tags
+}
+
+function main() {
+    #  Exit immediately if a command exits with a non-zero status.
+    # set -e
+    # Command options.
+    option="${1}"
+    message="${2}"
+
+    validate_arguments "$@"
+    get_last_version
+
+    if [ ${option} == "-c" ]; then
+        create_release_candidate "${message}"
+    elif [ ${option} == "-u" ]; then
+        upgrade_release_candidate
+    fi
+
+    return 0
+}
+
+main "$@"
