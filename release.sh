50d0e26dceff532e9c1edfbcb79b7a75cb4342f3
Timoteo Latisnere <tlatisnere@neology.mx>
2018-08-15 12:54:51 -0500

    [BN-342] fix Final corrections to release script

--- release.sh
+++ release.sh
@@ -12,6 +12,7 @@ IMPROVEMENT_TYPE="imp"
 FEATURE_TYPE="feat"
 GENERIC_TYPE="n/a"
 VERSION_FILE="version.txt"
+current_branch=$(git branch | grep -oP "\*\s\K.+")
 
 function print_usage() {
     echo "Usage: release.sh OPTION [ARGUMENTS...]" >&2
@@ -34,26 +35,37 @@ function validate_arguments() {
 }
 
 function get_last_version_data() {
+    local include_first_parent="${1}"
     # Recover version data from Git
-    last_tag=$(git describe --abbrev=0)
+    if [ ! -z ${1} ]; then
+        last_tag=$(git describe "${include_first_parent}" --abbrev=0)
+    else
+        last_tag=$(git describe --abbrev=0)
+    fi
     last_version=$(echo ${last_tag} | grep -oP "v\K\d+\.\d\.\d\.\d(?=[\-\w]*)")
     last_label=$(echo ${last_tag} | grep -oP "v\d+\.\d\.\d\.\d\-\K\w*")
     last_commit_message=$(git log --format=%B -n 1)
     last_commit_info=($(echo "${last_commit_message}" | tr ' ' '\n'))
     # Possible commit types: fix, hotfix, imp, feat, n/a
-    type=${last_commit_info[1]}
-    current_branch=$(git branch | grep -oP "\*\s\K.+")
-    # Extract version components in the format W.X.Y.Z
+    last_commit_type=${last_commit_info[1]}
+    # Extract git version components in the format W.X.Y.Z
     version_components=($(echo "${last_version}" | tr '.' '\n'))
+    # Recover version data from version file
+    tag_in_file=$(cat "${VERSION_FILE}")
+    label_in_file=$(echo "${tag_in_file}" | grep -oP "v\d+\.\d\.\d\.\d\-\K\w*")
+    version_in_file=$(echo "${tag_in_file}" | grep -oP "v\K\d+\.\d\.\d\.\d(?=[\-\w]*)")
+    # Extract file version components in the format W.X.Y.Z
+    version_components_in_file=($(echo "${version_in_file}" | tr '.' '\n'))
 }
 
 function update_version_file() {
     local new_version="${1}"
-    sed "s/${last_tag}/v${new_version}/" ${VERSION_FILE} > tmp.txt && mv tmp.txt ${VERSION_FILE}
+    sed "s/${tag_in_file}/v${new_version}/" ${VERSION_FILE} > tmp.txt && mv tmp.txt ${VERSION_FILE}
 }
 
 function create_release_candidate() {
     local tag_message="${1}"
+    get_last_version_data
     # Create a release candidate only when develop is the origin
     if [ ${current_branch} == ${DEVELOP_BRANCH} ]; then
         # Increase version 'X' component with each new release. Reset to zero 'Y' and 'Z' components.
@@ -76,34 +88,33 @@ function create_release_candidate() {
     fi
 }
 
-function scan_previous_commits(){
-    # Number of commits since last tag
-    commits_since_last_tag=$(git describe --long| grep -oP '\d+(?=\-[\w\d]{8}$)')
-    types_since_last_tag=($(git log --reverse -n "${commits_since_last_tag}" --format=%B | grep -oP '(?<=\]\s)[\w\/]+(?=\s)'))
-    for type in "${types_since_last_tag[@]}"
-    do :
-        if [ ${type} == ${FIX_TYPE} ] || [ ${type} == ${HOTFIX_TYPE} ]; then
-            # Increase version 'Y' component with each bug fix. Reset 'Z' to zero
-            let version_components[2]++
-            let version_components[3]=0
-        elif [ ${type} == ${IMPROVEMENT_TYPE} ] || [ ${type} == ${GENERIC_TYPE} ]; then
-            # Increase version 'Z' component with each improvement.
-            let version_components[3]++
-        elif [ ${type} == ${FEATURE_TYPE} ]; then
-            # New features should not be allowed in release branches.
-            echo "Commits associated to new functionality should not be applied to release branches." >&2
-            echo "Aborting release upgrade." >&2
-            exit 1
-        fi
-    done
+function upgrade_release_candidate() {
+    get_last_version_data
+    calculate_version
+    release_version=$(echo "${version_components[0]}.${version_components[1]}.${version_components[2]}.${version_components[3]}")
+    # Add rc suffix if branch being upgraded is a release candidate.
+    release_version=$(echo "${release_version}-${RELEASE_CANDIDATE_SUFFIX}")
+    update_version_file "${release_version}"
+    git add "${VERSION_FILE}"
+    git commit -m "[n/a] n/a Release ${release_version} upgrade applied."
+    last_commit_message=$(git log --format=%B -n 1)
+    git tag -a "v${release_version}" -m "${last_commit_message}"
+    git push origin "${current_branch}"
+    git push origin --tags
 }
 
 function upgrade_release() {
-    scan_previous_commits
-    release_version=$(echo "${version_components[0]}.${version_components[1]}.${version_components[2]}.${version_components[3]}")
-    # Add rc suffix if branch being upgraded is a release candidate.
-    if [[ ${current_branch} == *"rc"* ]]; then
-      release_version=$(echo "${release_version}-${RELEASE_CANDIDATE_SUFFIX}")  
+    # Recover data from previous master commits exclusively to analyze previous released version.
+    get_last_version_data --first-parent
+    # Determine if last commit corresponds to a rc or hotfix merge.
+    if [[ ${last_commit_message} == *"${DEVELOP_BRANCH}"* ]] \
+        || [[ ${last_commit_message} == *"${RELEASE_CANDIDATE_SUFFIX}"* ]]; then
+        # Merge comes from develop or release candidate, remove rc label and preserve version components.
+        release_version=$(echo "${version_in_file}")
+    elif [[ ${last_commit_message} == *"${HOTFIX_TYPE}"* ]]; then
+        # Merge comes from hotfix branch, increment 'Y'
+        let version_components[2]++
+        release_version=$(echo "${version_components[0]}.${version_components[1]}.${version_components[2]}.${version_components[3]}")
     fi
     update_version_file "${release_version}"
     git add "${VERSION_FILE}"
@@ -114,6 +125,46 @@ function upgrade_release() {
     git push origin --tags
 }
 
+
+function upgrade_version() {
+    # Number of commits since last tag
+    commits_since_last_tag=$(git describe --long| grep -oP '\d+(?=\-[\w\d]{8}$)')
+    if [ ${commits_since_last_tag} -eq 0 ]; then
+        echo "No changes since last version. Aborting upgrade."
+        exit 1
+    fi
+    if [[ "${current_branch}" == *"${RELEASE_CANDIDATE_SUFFIX}"* ]]; then
+      upgrade_release_candidate
+    elif [ "${current_branch}" == ${MASTER_BRANCH} ]; then
+      upgrade_release
+    fi
+}
+
+function calculate_version(){
+    types_since_last_tag=($(git log --reverse -n "${commits_since_last_tag}" --format=%B | grep -oP '(?<=\]\s)[\w\/]+(?=\s)'))
+    for last_commit_type in "${types_since_last_tag[@]}"
+    do :
+        if [ ${last_commit_type} == ${FIX_TYPE} ] || [ ${last_commit_type} == ${HOTFIX_TYPE} ]; then
+            # Increase version 'Y' component with each bug fix. Reset 'Z' to zero
+            let version_components[2]++
+            let version_components[3]=0
+        elif [ ${last_commit_type} == ${IMPROVEMENT_TYPE} ] || [ ${last_commit_type} == ${GENERIC_TYPE} ]; then
+            # Increase version 'Z' component with each improvement.
+            let version_components[3]++
+        elif [ ${last_commit_type} == ${FEATURE_TYPE} ]; then
+            # New features should not be allowed in release branches.
+            echo "Commits associated to new functionality should not be applied to release branches." >&2
+            echo "Aborting release upgrade." >&2
+            exit 1
+        else
+            # Reject any other commit format.
+            echo "Found incorrect syntax in commit messages since last version tag. Please review commit messages structure." >&2
+            echo "Aborting release upgrade." >&2
+            exit 1
+        fi
+    done
+}
+
 function main() {
     #  Exit immediately if a command exits with a non-zero status.
     # set -e
@@ -122,12 +173,11 @@ function main() {
     message="${2}"
 
     validate_arguments "$@"
-    get_last_version_data
 
     if [ ${option} == "-c" ]; then
         create_release_candidate "${message}"
     elif [ ${option} == "-u" ]; then
-        upgrade_release
+        upgrade_version
     fi
 
     return 0
